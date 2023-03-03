c08ec70dc9ad33b4465e915adfe753ecf4455ae4
John  Furr <jfurr@johns-mbp.lan>
2023-03-03 12:17:13 -0500

    App fully working in docker containers with CORS exception on backend for frontend

--- backend/main.py
+++ backend/main.py
@@ -10,20 +10,12 @@ from fastapi.middleware.cors import CORSMiddleware
 # app = FastAPI(dependencies=[Depends(get_query_token)])
 app = FastAPI()
 
-# origins = [
-#     "http://localhost.tiangolo.com",
-#     "https://localhost.tiangolo.com",
-#     "http://localhost",
-#     "http://localhost:3000",
-# ]
-
-# app.add_middleware(
-#     CORSMiddleware,
-#     allow_origins=origins,
-#     allow_credentials=True,
-#     allow_methods=["*"],
-#     allow_headers=["*"],
-# )
+#Allow CORS from frontend app on port 8001
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:8001"],
+    allow_headers=["Access-Control-Allow-Origin"]
+)
 
 # app.include_router(users.router)
 # app.include_router(items.router)
