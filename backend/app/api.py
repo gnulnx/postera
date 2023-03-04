74bfe1de7558d2c8d94d19743d4ec261fd05329a
John  Furr <jfurr@johns-mbp.lan>
2023-03-04 13:11:31 -0500

    Basic process_route to convert rxns to d3 graphs

--- backend/app/api.py
+++ backend/app/api.py
@@ -12,6 +12,9 @@ es = Elasticsearch("http://localhost:9200")
 
 app = FastAPI()
 
+def jprint(s):
+    print(json.dumps(s, indent=4))
+
 #Allow CORS from frontend app on port 8001
 app.add_middleware(
     CORSMiddleware,
@@ -26,10 +29,99 @@ app.add_middleware(
 )
 
 
-def make_routes():
-    # TODO: use this method to return routes as a tree data structure.
-    # routes are found in the routes.json file
-    return [{}]
+def process_route(route):
+    """
+        Given a synthetic route described as such
+
+        "reactions": [
+            {
+                "name": "Amidation",
+                "target": "O=C(Cn1nnc2ccccc21)NCc1ccsc1",
+                "sources": [
+                    "NCc1ccsc1",
+                    "O=C(O)Cn1nnc2ccccc21"
+                ],
+                "smartsTemplate": "[C:2](=[O:3])[N;!$(N(C=O)(C=O)):1]>>([N:1][#1].[C:2](=[O:3])[O][#1])"
+            },
+            {
+                "name": "Iodo N-arylation",
+                "target": "O=C(Cn1nnc2ccccc21)N(Cc1ccsc1)c1ccc(Cl)cc1",
+                "sources": [
+                    "Clc1ccc(I)cc1",
+                    "O=C(Cn1nnc2ccccc21)NCc1ccsc1"
+                ],
+                "smartsTemplate": "[c:1]-;!@[$(n),$([N][C]=[O]):2]>>([*:1][I].[*:2][#1])"
+            }
+        ],
+
+        Convert to a D3 suitable graph in the form
+
+        reactions = {
+            "name": 'O=C(Cn1nnc2ccccc21)N(Cc1ccsc1)c1ccc(Cl)cc1',
+            "attributes": {
+                "reaction": "Amidation"
+            },
+            "children": [
+                {
+                    "name": 'Clc1ccc(I)cc1"',
+                    "attributes": {
+                    "reaction": 'Iodo N-arylation',
+                    }
+                },
+                {
+                    "name": 'O=C(Cn1nnc2ccccc21)NCc1ccsc1',
+                    "children": [
+                        {
+                            "name": 'NCc1ccsc1',
+                        },
+                        {
+                            "name": "O=C(O)Cn1nnc2ccccc21"
+                        }
+                    ]
+                },
+            ],
+        }
+    """
+    route = route["_source"]
+    products = [mol for mol in route["molecules"] if mol["is_building_block"] is False]
+    building_blocks = [mol for mol in route["molecules"] if mol["is_building_block"] is True]
+
+    print(f"This route has {len(products)} products and {len(building_blocks)} building blocks")
+    for mol in products:
+        print(mol)
+
+    rxn_map = {}
+    for product in route["reactions"]:
+        children = []
+        for s in product["sources"]:
+            if s in rxn_map:
+                children.append({
+                    "name": s,
+                    "attributes": rxn_map[s]["attributes"],
+                    "children": rxn_map[s]["children"]
+                })
+            else:
+                children.append({"name": s})
+
+        rxn_tree = {
+            "name": product["target"],
+            "attributes": {"reaction": product["name"]},
+            "children": children
+        }
+
+        
+        # rxn["children"] = children
+
+        # jprint(rxn)
+
+        rxn_map[product["target"]] = rxn_tree
+        # routes.append(rxn_tree)
+
+    # jprint(route["reactions"])
+    # print("reactions above")
+    # jprint(rxn)
+    # print("final graph above")
+    return [rxn_tree]
 
 def draw_molecule(smiles: str):
     mol = Chem.MolFromSmiles(smiles)
@@ -56,56 +148,7 @@ async def get_routes(q: str) -> dict:
     # "Amidation molport O=C(O)Cn1nnc2ccccc21"
     # A Basic ES query to get searchable routes quickly.
     # May need field boosting for better peformance
-    query = {
-        "multi_match": {
-            "query": q,
-            "fields": [
-                "molecules.catalog_entries.catalog_name",
-                "reactions.name",
-                "reactions.target",
-                "reactions.sources"
-            ]
-        }
-    }
-
-    terms = q.split()
-    print(terms)
-
-    blah = [{
-        "multi_match": {
-            "query": term,
-            "fields": [
-                "molecules.catalog_entries.catalog_name",
-                "reactions.name",
-                "reactions.target",
-                "reactions.sources"
-            ]
-        }
-    }
-    for term in q.split()]
-
-    print(blah)
-
-    query = {
-        "bool": {
-            "must": [
-                {
-                    "multi_match": {
-                        "query": q,
-                        "fields": [
-                            "molecules.catalog_entries.catalog_name",
-                            "reactions.name",
-                            "reactions.target",
-                            "reactions.sources"
-                        ],
-                        "fuzziness" : "AUTO",
-                        "prefix_length" : 2
-                    }
-                }
-            ]
-        }
-    }
-
+  
     query = {
         "bool": {
             "must": [
@@ -127,7 +170,6 @@ async def get_routes(q: str) -> dict:
             ]
         }
     }
-
     highlight = {
         "fields" : {
             "molecules.catalog_entries.catalog_name": {},
@@ -137,19 +179,16 @@ async def get_routes(q: str) -> dict:
         }
     }
 
-    print(json.dumps(query, indent=4))
+    # print(json.dumps(query, indent=4))
 
-    resp = es.search(index="routes", query=query, highlight=highlight)
-    print(resp.keys())
-    
-    # for doc in resp["hits"]["hits"]:
-    #     print(doc)
-
-    print(json.dumps(resp["hits"]["hits"], indent=4))
+    resp = es.search(index="routes", query=query, highlight=highlight, size=100)
 
     total_results = resp['hits']['total']['value']
     print(f"Total hits {total_results}")
-    routes = make_routes()
+
+    for route in resp["hits"]["hits"]:
+        rxn = process_route(route)
+    # routes = make_routes()
 
     """
      /*
@@ -176,31 +215,57 @@ async def get_routes(q: str) -> dict:
   */
     """
 
-    reactions = {
-        "name": 'O=C(Cn1nnc2ccccc21)N(Cc1ccsc1)c1ccc(Cl)cc1',
-        "attributes": {
-        "reaction": "Amidation"
-        },
-        "children": [
-        {
-            "name": 'Clc1ccc(I)cc1"',
-            "attributes": {
-            "reaction": 'Iodo N-arylation',
-            }
-        },
-        {
-            "name": 'O=C(Cn1nnc2ccccc21)NCc1ccsc1',
-            "children": [
-            {
-                "name": 'NCc1ccsc1',
-            },
-            {
-                "name": "O=C(O)Cn1nnc2ccccc21"
-            }
-            ]
-        },
-        ],
-    }
+    # reactions = {
+    #     "name": 'O=C(Cn1nnc2ccccc21)N(Cc1ccsc1)c1ccc(Cl)cc1',
+    #     "attributes": {
+    #     "reaction": "Amidation"
+    #     },
+    #     "children": [
+    #     {
+    #         "name": 'Clc1ccc(I)cc1"',
+    #         "attributes": {
+    #         "reaction": 'Iodo N-arylation',
+    #         }
+    #     },
+    #     {
+    #         "name": 'O=C(Cn1nnc2ccccc21)NCc1ccsc1',
+    #         "children": [
+    #         {
+    #             "name": 'NCc1ccsc1',
+    #         },
+    #         {
+    #             "name": "O=C(O)Cn1nnc2ccccc21"
+    #         }
+    #         ]
+    #     },
+    #     ],
+    # }
+
+    # rxn = {
+    #     "name": "O=C(Cn1nnc2ccccc21)N(Cc1ccsc1)c1ccc(Cl)cc1",
+    #     "attributes": {
+    #         "reaction": "Buchwald-Hartwig amination with amide"
+    #     },
+    #     "children": [
+    #         {
+    #             "name": "Clc1ccc(I)cc1"
+    #         },
+    #         {
+    #             "name": "O=C(Cn1nnc2ccccc21)NCc1ccsc1",
+    #             "attributes": {
+    #                 "reaction": "Amide Schotten-Baumann"
+    #             },
+    #             "children": [
+    #                 {
+    #                     "name": "NCc1ccsc1"
+    #                 },
+    #                 {
+    #                     "name": "O=C(Cl)Cn1nnc2ccccc21"
+    #                 }
+    #             ]
+    #         }
+    #     ]
+    # }
     return {
-        "data": reactions,
+        "data": rxn,
     }
