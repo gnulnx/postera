6839b6b19e28b5bd4f8348c2918d8de1e1029490
John  Furr <jfurr@johns-mbp.lan>
2023-03-06 12:51:11 -0500

    Remove comment

--- backend/main.py
+++ backend/main.py
@@ -1,6 +1,4 @@
 import uvicorn
 
 if __name__ == "__main__":
-    # Use first when containerized
-    # uvicorn.run("app.api:app", host="0.0.0.0", port=8000, reload=True)
     uvicorn.run("app.api:app", host="0.0.0.0", port=8080, reload=True)
