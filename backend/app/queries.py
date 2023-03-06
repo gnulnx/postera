ccfc4ee2f1d66d3669c18debb2f29204ba3f0e9e
John  Furr <jfurr@johns-mbp.lan>
2023-03-06 05:03:51 -0500

    hold on branch bb_refinedment

--- backend/app/queries.py
+++ backend/app/queries.py
@@ -47,10 +47,12 @@ def typeahead_search(q):
     results = []
     for route in resp["hits"]["hits"]:
 
+        # Build a list of building blocks from the molecules section
         building_blocks = [mol for mol in route["_source"]["molecules"] if mol["is_building_block"] is True]
+        # total_bbs = len(building_blocks)
 
         # Check if the user is trying to filter based on vender name from the highlights.
-        vendor_highlights = route["highlight"].get("molecules.catalog_entries.catalog_name")            
+        vendor_highlights = route.get("highlight").get("molecules.catalog_entries.catalog_name")            
 
         filtered_bbs = []
         if vendor_highlights:
@@ -58,20 +60,16 @@ def typeahead_search(q):
             preferred_vendors = {v.replace("<em>","").replace("</em>", "") for v in vendor_highlights}
 
             for mol in building_blocks:
-                new_mol = {
-                    "smiles": mol["smiles"],
-                    "catalog_entries": []
-                }
-                for entry in mol["catalog_entries"]:
-                    if entry["catalog_name"] in preferred_vendors:
-                        new_mol["catalog_entries"].append(entry)
+                mol["catalog_entries"] = [entry for entry in mol['catalog_entries'] if entry["catalog_name"] in preferred_vendors]
+                # if not mol["catalog_entries"]:
+                #     continue
+                if mol["catalog_entries"]:
+                    filtered_bbs.append(mol)
 
-                # If we have any catalog_entries then we keep the new molecule otherwise we don't.
-                if new_mol["catalog_entries"]:
-                    filtered_bbs.append(new_mol)
+        #     jprint(building_blocks)
 
-            # Our filtered list of buildings blocks should have 'less' catalog_entries, but the total
-            # number of building blocks should still be the same.
+        #     # Our filtered list of buildings blocks should have 'less' catalog_entries, but the total
+        #     # number of building blocks should still be the same.
             if len(building_blocks) == len(filtered_bbs):
                 results.append({
                     "score": route["_score"],
@@ -89,6 +87,24 @@ def typeahead_search(q):
             "rxn_name": [rxn["name"] for rxn in route["_source"]["reactions"]],
             "building_blocks":  building_blocks
         })
+        
+        # results.append({
+        #     "score": route["_score"],
+        #     "id": route["_id"],
+        #     "rxn_name": [rxn["name"] for rxn in route["_source"]["reactions"]],
+        #     "building_blocks":  building_blocks
+        # })
+
+            
+        # results = [
+        #     {
+        #         "score": route["_score"],
+        #         "id": route["_id"],
+        #         "rxn_name": [rxn["name"] for rxn in route["_source"]["reactions"]],
+        #         "building_blocks":  building_blocks
+        #     }
+        #     for mol in building_blocks if mol["catalog_entries"]
+        # ]
 
     return results
 
