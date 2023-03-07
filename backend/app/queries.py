6d4a78659a42c897d34ced09221c9a312b37189f
John  Furr <jfurr@johns-mbp.lan>
2023-03-07 11:57:50 -0500

    Slight cleanup

--- backend/app/queries.py
+++ backend/app/queries.py
@@ -16,25 +16,33 @@ import rdkit.Chem as Chem
 def typeahead_search(q: str):
     """
     Perform an intelligent fuzzy typeahead search
+    3 parts.
+    1. Preprocess query terms into 3 categories
+      - terms that we will use do fuzzy autocomplete on
+      - terms that we will do exact matches on.
+      - Terms used to postprocess results based on vendor
+    2. Elastic Search Query
+    3. Post Processing Results
+
     """
 
+    # 1) Preprocess query terms.
     terms = q.split()
 
-    # Check for negated vedors
+    # Negated vendor list used in postprocessing
     negated_vendors = [term for term in terms if term.startswith("-")]
     
-    # Check our list of terms for smiles strings.
-    smiles = terms_to_smiles(terms)
+    # For this simple app exact matches are any smiles string detected in the input
+    exact_match_terms = terms_to_smiles(terms)
 
-    # Remove all smiles strings and negated_vendors from our terms
-    for term in smiles + negated_vendors:
-        terms.remove(term)
+    # Remove negated vendors and exact match terms to leave list of fuzzy autocomplete terms 
+    multi_match_terms = set(terms) - set(negated_vendors + exact_match_terms)
 
     # Now remove the leading '-' from negated vendors for easier matching later
     negated_vendors = [vendor[1:] for vendor in negated_vendors]
     
-    # Build fuzzy autocomplete queries for text terms
-    text_queries = [{
+    # 2) Build up ES queries and fetch results from ES
+    multi_match_query = [{
         "multi_match": {
             "query": term,
             "fields": [
@@ -45,21 +53,21 @@ def typeahead_search(q: str):
             "prefix_length" : 3,
             "_name": "matched_field"
         }
-    } for term in terms] 
+    } for term in multi_match_terms] 
     
 
     # Build exact match queries for bbs matching smiles
-    bb_queries = [{
+    exact_match_query = [{
         "term": {
             "reactions.sources.keyword": {
-                "value": mol
+                "value": term
             }
         }
-    } for mol in smiles]
+    } for term in exact_match_terms]
     
     query = {
         "bool": {
-            "must": text_queries + bb_queries,
+            "must": multi_match_query + exact_match_query,
         }
     }
     highlight = {
@@ -69,40 +77,43 @@ def typeahead_search(q: str):
             "reactions.sources.keyword": {}
         }
     }
+    jprint(query)
 
     resp = es.search(index="routes", query=query, highlight=highlight, source=["molecules", "reactions"], size=100)
 
+    # 3) Post process the results
     results = []
     for route in resp["hits"]["hits"]:
+        molecules = route["_source"]["molecules"]
+        reactions = route["_source"]["reactions"]
 
         # Build a list of building blocks from the molecules section
-        building_blocks = [mol for mol in route["_source"]["molecules"] if mol["is_building_block"] is True]
+        bbs = [mol for mol in  molecules if mol["is_building_block"] is True]
 
-        # Remove all the negated vendors from the building_blocks
-        for bb in building_blocks:
+        # Remove negated vendors from building block catalog_entries
+        for bb in bbs:
             bb["catalog_entries"] = [entry for entry in bb["catalog_entries"] if entry["catalog_name"] not in negated_vendors]
 
         # Check if the user is trying to filter based on vender name from the highlights.
-        vendor_highlights = route.get("highlight", {}).get("molecules.catalog_entries.catalog_name")
+        # If so create a list of preferred_vendors
+        vendor_highlights = route.get("highlight", {}).get("molecules.catalog_entries.catalog_name", [])
+        preferred_vendors = {v.replace("<em>","").replace("</em>", "") for v in vendor_highlights}
 
-        found_vendors_for_all_bb = True
-        if vendor_highlights:
-            # Create a set of preferred vendors from the search results
-            preferred_vendors = {v.replace("<em>","").replace("</em>", "") for v in vendor_highlights}
-
-            for mol in building_blocks:
+        vendors_for_all_bb = True
+        if preferred_vendors:
+            for mol in bbs:
                 mol["catalog_entries"] = [entry for entry in mol['catalog_entries'] if entry["catalog_name"] in preferred_vendors]
                 if not mol["catalog_entries"]:
-                    found_vendors_for_all_bb = False
+                    vendors_for_all_bb = False
                     break
             
-        if found_vendors_for_all_bb:
+        if vendors_for_all_bb:
             results.append({
                 "score": route["_score"],
                 "id": route["_id"],
-                "rxn_name": [rxn["name"] for rxn in route["_source"]["reactions"]],
-                "building_blocks":  building_blocks,
-                "est_lead_time": est_lead_time(building_blocks)
+                "rxn_name": [rxn["name"] for rxn in reactions],
+                "building_blocks":  bbs,
+                "est_lead_time": est_lead_time(bbs)
             })
 
     return results
